f7834c5d64af1e529057050db3398a4837efa59e
Vittor-Javidan <vittor.javidan.t@gmail.com>
2022-12-18 08:21:49 -0300

    Refactor: Auction module, chatListeners.js

--- src/features_modules/Auction/chatListeners.js
+++ src/features_modules/Auction/chatListeners.js
@@ -2,80 +2,84 @@ import tmi from 'tmi.js'
 import Auction from './Auction'
 
 /**
- * @param {string} channel 
- * @param {string} message 
+ * @param {string} channel
+ * @param {string} message
  * @param {boolean} self
- * @param {tmi.Client} client 
+ * @param {tmi.Client} client
  */
 function createAuction(channel, message, self, client) {
+	if (
+		message.toLowerCase().includes('!leilão') &&
+		!message.toLowerCase().includes('timeleft')
+	) {
+		
+		//Error handler
+		if (!stringValidation(message)) {
+			client.say(channel, 'Commando inválido seu noob!')
+			return
+		}
 
-    if(message.toLowerCase().includes('!leilão') && !message.toLowerCase().includes('timeleft')) {
-
-        //Error handler
-        if(!stringValidation(message)) {
-            client.say(channel, 'Commando inválido seu noob!')
-            return
-        }
-
-        const item = message.split(' ')[1]
-        const minutes = Number(message.split(' ')[2])
-        Auction.init({
-            item: item,
-            minutes: minutes,
-            channel: channel,
-            client: client
-        })
-    }
+		const item = message.split(' ')[1]
+		const minutes = Number(message.split(' ')[2])
+		Auction.init({
+			item: item,
+			minutes: minutes,
+			channel: channel,
+			client: client,
+		})
+	}
 }
 
 /**
- * @param {string} channel 
- * @param {string} message 
+ * @param {string} channel
+ * @param {string} message
  * @param {boolean} self
- * @param {tmi.Client} client 
+ * @param {tmi.Client} client
  */
 function setAuctionTimeLeft(channel, message, self, client) {
 
-    if(message.toLowerCase().includes('!leilão timeleft')) {
+	if (message.toLowerCase().includes('!leilão timeleft')) {
 
-        if(Auction.auctions.length > 0) {
+		const minutes = Number(message.split(' ')[2])
 
-            const minutes = Number(message.split(' ')[2])
-            if(minutes !== NaN) {
-                Auction.getInstance().minutes = minutes
-                client.say(channel, `Tempo restante do Leilão[${Auction.getInstance().item}] modificado para ${minutes} minutos`)
-            } else {
-                client.say(channel, `Comando inválido!`)
-            }
-            
-        } else {
-            client.say(channel,
-                `Não há nenhum leilão em andamento no momento`    
-            )
-        }
-    }
+		if (Auction.auctions.length <= 0) {
+			client.say(channel, `Não há nenhum leilão em andamento no momento`)
+			return
+		}
 
+		if (isNaN(minutes)) {
+			client.say(channel, `Comando inválido!`)
+			return
+		}
+
+		Auction.getInstance().minutes = minutes
+		client.say(
+			channel,
+			`Tempo restante do Leilão[${
+				Auction.getInstance().item
+			}] modificado para ${minutes} minutos`
+		)
+	}
 }
 
 const auctionChatListeners = {
-    createAuction,
-    setAuctionTimeLeft
+	createAuction,
+	setAuctionTimeLeft,
 }
 export default auctionChatListeners
 
 /**
- * @param {string} message 
+ * @param {string} message
  * @returns {boolean}
  */
 function stringValidation(message) {
+	const messageWords = message.split(' ')
 
-    const messageWords = message.split(' ')
+	//Check if the string has lenght 4
+	if (messageWords.length !== 3) return false
 
-    //Check if the string has lenght 4
-    if( messageWords.length !== 3 ) return false
+	//Verify if the last word is a valid number
+	if (isNaN(messageWords[2])) return false
 
-    //Verify if the last word is a valid number
-    if( isNaN(messageWords[2]) ) return false
-
-    return true
+	return true
 }
