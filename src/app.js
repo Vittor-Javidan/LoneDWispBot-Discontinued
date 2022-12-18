7482a516782fe1af52e3cda073f246317553b94f
Vittor-Javidan <vittor.javidan.t@gmail.com>
2022-12-18 06:38:21 -0300

    Added bids (500-10000) for Auction Module

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import env from './env'
 import client from './connect'
 
 //Chat Listeners
-import websiteChatListeners from './features_modules/Website/chatCommand'
+import websiteChatListeners from './features_modules/Website/chatListeners'
 import leilaoChatListeners from './features_modules/Leilao/chatListeners'
 
 //Reward Listeners
+import websiteRewardListeners from './features_modules/Website/rewardListeners'
 import musicaRewardListeners from './features_modules/Musica/rewardListeners'
 import leilaoRewardListeners from './features_modules/Leilao/rewardListeners'
 
@@ -26,9 +27,14 @@ client.on('redeem', (channelName, userName, rewardIdentifier) => {
 
 	try {
 		console.log(rewardIdentifier)
+		websiteRewardListeners.website(channelName, userName, rewardIdentifier, client)
 		musicaRewardListeners.adicioneMinhaMusica(channelName, userName, rewardIdentifier, client)
-		leilaoRewardListeners.leilaoBid100(channelName, userName, rewardIdentifier, client)
 		leilaoRewardListeners.leilaoRank(channelName, userName, rewardIdentifier, client)
+		leilaoRewardListeners.leilaoBid100(channelName, userName, rewardIdentifier, client)
+		leilaoRewardListeners.leilaoBid500(channelName, userName, rewardIdentifier, client)
+		leilaoRewardListeners.leilaoBid1000(channelName, userName, rewardIdentifier, client)
+		leilaoRewardListeners.leilaoBid5000(channelName, userName, rewardIdentifier, client)
+		leilaoRewardListeners.leilaoBid10000(channelName, userName, rewardIdentifier, client)
 	} catch(err) {
 		client.say(channelName, `Ocorreu um erro na recompensa resgatada de @${userName}. Id da recompensa: ${rewardIdentifier} `)
 	}
