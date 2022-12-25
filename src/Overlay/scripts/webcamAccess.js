6352b147cf811afb041aee433091817c9b5ca3b0
Vittor-Javidan <vittor.javidan.t@gmail.com>
2022-12-25 19:30:00 -0300

    REFACTOR: Overlay
    	- some webcam css changed

--- src/Overlay/scripts/webcamAccess.js
+++ src/Overlay/scripts/webcamAccess.js
@@ -4,6 +4,7 @@
 */
 
 let video = document.querySelector('#webcam');
+let videoContainer = document.querySelector('#webcam-container')
 
 if(navigator.mediaDevices.getUserMedia) {
 	navigator.mediaDevices.getUserMedia({ video: { width:500, height: 500 } })
@@ -12,3 +13,33 @@ if(navigator.mediaDevices.getUserMedia) {
 } else {
 	console.log('getUserMedia not supported!')
 }
+
+// Get the video element and the two buttons
+const invertButton = document.getElementById('invert-button');
+const hideButton = document.getElementById('hide-button');
+
+// Add click event listener to the invert button
+invertButton.addEventListener('click', () => {
+
+    // Check if the video is currently inverted
+    if (video.style.transform === 'rotateY(180deg)') {
+        // If it is, reset the transform to its original state
+        video.style.transform = 'none';
+    } else {
+        // If it is not, rotate the video 180deg
+        video.style.transform = 'rotateY(180deg)';
+    }
+});
+
+// Add click event listener to the hide button
+hideButton.addEventListener('click', () => {
+
+    // Check if the video is currently hidden
+    if (videoContainer.style.display === 'none') {
+    // If it is, show the video
+    videoContainer.style.display = 'block';
+    } else {
+    // If it is not, hide the video
+    videoContainer.style.display = 'none';
+    }
+});
