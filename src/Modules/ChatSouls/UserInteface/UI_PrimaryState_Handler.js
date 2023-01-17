3d8b770ae5c5cb7007b8bf103534bb32a9befc55
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-01-16 21:28:42 -0300

    REFACTOR: Chatsouls, UI_PrimaryState_Handler
    	- Added a try catch to send player to firepit in case there is
    	  any throw errors during player interaction.

--- src/Modules/ChatSouls/UserInteface/UI_PrimaryState_Handler.js
+++ src/Modules/ChatSouls/UserInteface/UI_PrimaryState_Handler.js
@@ -2,6 +2,7 @@ import Player from "../Classes/EntityChilds/Player";
 import PLAYER_STATES from "../Classes/EntityChilds/PLAYER_STATES";
 import UI_Exploring_Handler from "./secondaryState_Handlers/UI_Exploring_Handler";
 import UI_FirePit_Handler from "./secondaryState_Handlers/UI_FirePit_Handler";
+import { sendMessage_UI_FirePit } from "./sendMessage_Customized/sendMessage_UI_firePit";
 
 /**
  * Handle !cs play commands accoding to players primary state
@@ -13,9 +14,27 @@ import UI_FirePit_Handler from "./secondaryState_Handlers/UI_FirePit_Handler";
 export default function gameUIHandler(data){
 
     const playerState = data.playerInstance.currentState
-    
-    switch (playerState.primary){
-        case PLAYER_STATES.FIRE_PIT.PRIMARY:   UI_FirePit_Handler(data)     ;break
-        case PLAYER_STATES.EXPLORING.PRIMARY:  UI_Exploring_Handler(data)   ;break
+
+    try {
+        
+        switch (playerState.primary){
+            case PLAYER_STATES.FIRE_PIT.PRIMARY:   UI_FirePit_Handler(data)     ;break
+            case PLAYER_STATES.EXPLORING.PRIMARY:  UI_Exploring_Handler(data)   ;break
+        }
+
+    } catch (error) {
+        
+        console.log(error)
+        sendMessage_UI_FirePit(
+            data.playerInstance,
+            `Ocorreu um erro no jogo e você foi mandado de volta a fogueira.`
+        )
+
+        data.playerInstance.ressurrect()
+        data.playerInstance.recoverHP()
+        data.playerInstance.currentState = {
+            primary: PLAYER_STATES.FIRE_PIT.PRIMARY,
+            secondary: PLAYER_STATES.FIRE_PIT.SECONDARY.RESTING_ON_FIRE_PIT
+        }
     }
 }
